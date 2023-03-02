97d4d4bbb592dc959e939432058e3471ed00378c
Oleksandr <dembele121212@gmail.com>
2023-03-02 19:18:48 +0200

    first commit

---
+++ 01-homework/script.js
@@ -0,0 +1,52 @@
+const milkPrice = 15.678;
+const  applePrice = 123.965;
+const  saloPrice = 90.2345;
+console.log(milkPrice,applePrice,saloPrice);
+
+// Максимальне число
+let maxPrice=Math.max(milkPrice,applePrice,saloPrice)
+console.log("Максимальна ціна: " , (maxPrice));
+
+// Мінімальне число
+let minPrice=Math.min(milkPrice,applePrice,saloPrice);
+console.log("Мінімальна ціна: " , (milkPrice));
+
+// Сума
+let sum = milkPrice + applePrice + saloPrice;
+console.log("Сума за всі товари: " , (sum));
+
+// Без копійок
+
+console.log("Ціна за товар без копійок: " , (Math.trunc(milkPrice),Math.trunc(applePrice),Math.trunc(saloPrice)));
+
+// Сума з округленням в меншу сторону
+let sumTrunc=Math.trunc(milkPrice) + Math.trunc(applePrice) + Math.trunc(saloPrice)
+let sumAroundfloor = Math.floor(sumTrunc);
+console.log("Сума товару без копійок, округлена в меншу сторону: "+(sumAroundfloor));
+
+
+// Виведення суми з округленням в до 100-х
+let sumAroundHundred=(Math.ceil(sumAroundfloor/100))*100;
+console.log("Сума товару округлена до 100-х: " ,(sumAroundHundred));
+
+// Булеве значення суми
+console.log("Булеве значення суми: " , Boolean(sumAroundfloor%2));
+// Сума решти без округлень
+const cash= 500;
+let equals =cash-sum ;
+console.log("Решта: " , (equals));
+//Середнє значення суми всіх товарів 
+let avaregeSum=sum/3;
+console.log("Середнє значення: " , (avaregeSum).toFixed(2));
+
+// Знижка
+const discountRandom=Math.random();
+console.log("Знижка: " , discountRandom);
+// Знижка та сума з округленням до 2-х знаків
+let milkPricediscount=(milkPrice-((milkPrice*discountRandom))).toFixed(2);
+console.log("Ціна на товар зі знижкою ",milkPricediscount);
+// Чистий прибуток
+const income=(applePrice/2)-(applePrice*discountRandom);
+console.log("Чистий прибуток: " , income); 
+// 
+console.log("Максимальна ціна: " + (maxPrice),"Мінімальна ціна: " + (minPrice),"Вартість всіх товарів: " + (sum),"Сума округлена в меншу сторону: " + (sumAroundfloor), "Сума товару округлена до 100-х: " + (sumAroundHundred) , "Решта: " + (equals) , "Середнє значення: " + (avaregeSum).toFixed(2));
---
+++ 01-lesson/script.js
@@ -0,0 +1,125 @@
+// console.log("It's working");
+// console.error("Somthing wrong");
+// console.warn(data:"HHH");
+
+
+// Zminni:
+
+// var let const
+
+// var oldVariabled = "Something";
+// console.log(oldVariabled);
+// {
+
+// }
+
+
+// let newVariabled;
+
+// const priceOfbook = 45;
+
+
+
+// camel case    let ageOfage;
+// snake_case    let age_of_table;
+
+
+
+// Typu
+
+// number
+// bigint
+// boolean
+// stirng
+// null     prusvoyetsya
+// undefind (default)
+// symbol
+
+// let age =10;
+// let isOld = false;
+// let name = "Tomas";
+
+// let empty = null;
+
+// object
+// const Person = {
+//     age: "20",
+//     name: ""
+// }
+// console.log(typeof empty)
+
+
+// alert();
+// prompt();
+// confirm();
+
+// alert("Hello!!");
+
+// prompt("Enter name");
+// const newName = prompt("Name");
+// console.log (newName + 20);
+
+// const result = confirm("Are you agree?");
+// console.log(result);
+
+// const age = 15;
+
+// if (age < 18) {
+//     alert ("Zaboroneno")
+// } else {
+//     alert ("Dozvoleno")
+// }
+// console.log(typeof alert);
+
+// const age =20;
+// // const isPresent = true;
+// const isPresent = false;
+
+// console.log(String(age),age);
+// console.log(String(isPresent),isPresent);
+// console.log(Number(isPresent))
+
+
+// const a = 10;
+// const b= 6;
+//  % dilennya z ostacheu
+// console.log( a % b);
+// console.log(a*b+a-a/b);
+
+// const a= "10";
+// const b= "3";
+// const result= a+b;
+// console.log(result)
+
+// const a=3.8;
+// const b = 3.1;
+// const num = 0.1 + 0.2;
+
+// console.log(Math.floor(a));
+// console.log(Math.ceil(a));
+// console.log(Math.round(a),Math.round(b));
+// console.log(num === 0.3);
+
+// console.log(parseInt( "2222zkdjsldj"));
+
+
+
+// const slovo = "This Is Our String";
+
+// const dogName = "Norty";
+// const dogAge = "3";
+// const result = `this is ${  dogAge > 10 ? "old dog " : "young dog"}`;
+// // ${  dogAge > 10 ? "old dog " : "young dog"} trenarnyy operator (? )
+
+
+// console.log(result);
+
+//  const result2 = "this is" + dogName + "'s food";
+
+ 
+
+
+// console.log( slovo.charAt(6));
+// console.log(slovo.indexOf("a"));
+
+
