2e6313acf8b923a998c8e093fc4f4a33cc768254
Oleksandr <dembele121212@gmail.com>
2023-03-28 22:22:40 +0300

    hw-7 add

---
+++ lesson-9/script.js
@@ -0,0 +1,14 @@
+let element_1 = document.getElementById('take_my_text')
+let element_2 = document.getElementsByClassName('take_my_texts')
+let element_3 = document.getElementsByName('take_my_input')
+let element_4 = document.getElementsByTagName('p')
+// let element_5 = document.getElementsByTagNameNS('take_my_texts')
+
+
+console.log(element_1)
+console.log(element_2.length)
+console.log(element_3)
+console.log(element_4)
+// console.console(element_5)
+// console.console(element_1)
+// console.console(element_1)
